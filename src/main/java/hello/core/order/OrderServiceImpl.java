b644512535d19d34e3caf94fe61decef07dffe7a
dnflekals <dnflekals@gmail.com>
2021-12-12 13:29:29 +0900

    feat: @Primary 대신 애노테이션 직접 만들어 적용하기

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -15,9 +16,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository,
-        DiscountPolicy DiscountPolicy) {
+        @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
-        this.discountPolicy = DiscountPolicy;
+        this.discountPolicy = discountPolicy;
     }
 
     @Override
